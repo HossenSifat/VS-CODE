bb2d84451374ff6717417ba8867ad691d46f4a00
Sifat-Hossen <md.sifathossen6100@gmail.com>
2022-08-05 00:05:29 +0600

    04/08/2022

--- Python/Basic.py
+++ Python/Basic.py
@@ -585,4 +585,79 @@ class Student(person):
 
 student1 = Student("A",20, 6.2,"ka@gmail.com", "234EEE" )
 student1.set_name("mofrat")
-print(student1.get_summary())       
+print(student1.get_summary()) 
+  
+#Data structure & Algorithm
+
+#link-list
+
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.val = value
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head =None
+        self.tail = None
+        self.size = 0
+    
+    def add(self, value):
+        node = Node(val)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+            self.size += 1
+    
+    def __remove_node(self, node):
+        if node.prev is None:
+            self.head = node.next
+        else:
+            node.prev.next = node.next
+
+        if node.next is None:
+            self.tail = node.prev
+        else:
+            node.next.prev = node.prev
+        self.size -= 1
+    
+    def remove(self, value):
+        node = self.head
+        while node is not None:
+            if node.val == value:
+                self.__remove_node(node)
+            node = node.next
+
+    def __str__(self):
+        vals=[]
+        node = self.head
+        while node is not None:
+            vals.append(node.val)
+            node = node.next
+        return f"[{','.join(str(val) for val in vals)}]"
+
+my_list = DoubleLinkedList()
+my_list.add(1)
+my_list.add(4)
+my_list.add(3)
+my_list.add(9)
+my_list.add(7)
+my_list.add(13)
+my_list.add(3)
+my_list.add(6)
+
+print(my_list)
+print(my_list.size)
+my_list.remove(4)
+print(my_list)
+
+
+
+
+
