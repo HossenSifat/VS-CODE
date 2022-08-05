273109b694591b75b29735cb024cefb6b2bd4eee
Sifat-Hossen <md.sifathossen6100@gmail.com>
2022-08-06 00:21:37 +0600

    05/08/22second

--- Python/DS1.py
+++ Python/DS1.py
@@ -1,5 +1,6 @@
 #link-list
 
+
 class Node:
     def __init__(self, value):
         self.next = None
@@ -23,6 +24,12 @@ class DoubleLinkedList:
             node.prev = self.tail
             self.tail = node
             self.size += 1
+    
+    def front(self):
+        return self.head.val
+    
+    def back(self):
+        return self.tail.val
     
     def __remove_node(self, node):
         if node.prev is None:
@@ -51,17 +58,55 @@ class DoubleLinkedList:
             node = node.next
         return f"[{','.join(str(val) for val in vals)}]"
 
-my_list = DoubleLinkedList()
-my_list.add(1)
-my_list.add(4)
-my_list.add(3)
-my_list.add(9)
-my_list.add(7)
-my_list.add(13)
-my_list.add(3)
-my_list.add(6)
+# my_list = DoubleLinkedList()
+# my_list.add(1)
+# my_list.add(4)
+# my_list.add(3)
+# my_list.add(9)
+# my_list.add(7)
+# my_list.add(13)
+# my_list.add(3)
+# my_list.add(6)
 
-print(my_list)
-print(my_list.size)
-my_list.remove(4)
-print(my_list)
+# print(my_list)
+# print(my_list.size)
+# my_list.remove(4)
+# print(my_list)
+
+#stack
+
+class Stack:
+    def __init__(self):
+        self.__list = DoubleLinkedList()
+
+    def push(self, val):
+        self.__list.add(val)
+
+    def pop(self):
+        val = self.__list.back()
+        self.__list.remove(val)
+        return val
+
+    def is_empty(self):
+        return self.__list.size == 0
+
+    def peak(self):
+        return self.__list.back()
+
+    def __len__(self):
+        return self.__list.size
+
+
+
+my_stack = Stack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+my_stack.push(4)
+print(my_stack.peak())
+my_stack.push(5)
+my_stack.push(8)
+print(len(my_stack))
+print(my_stack.pop())
+print(len(my_stack))
+print(my_stack.peak())
